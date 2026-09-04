cf2b4b49338c617676c656398ff72314ba654305
Mekudi84 <unyimebassey84@gmail.com>
2026-09-04 10:50:46 +0100

    Wire react-router-dom and prepare entrypoint for v2 dashboard route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
+import Dashboard from "./v2/Dashboard.jsx";
 import "../css/style.css";
+import "../css/v2.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/v2/*" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
